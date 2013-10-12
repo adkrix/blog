c4af4dd0eee32799a42d8f440b52953ccecb7cc1
Arthur Creek <arthur.creek@gmail.com>
2013-10-12 15:58:43 +0300

    add devise views

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
-describe Post do
-  it { should have_many :comments }
+describe Comment do
+  it { should belong_to :post }
 
-  it { should validate_presence_of :title }
   it { should validate_presence_of :body }
+
+  it { should have_db_column(:body).of_type(:text) }
+  it { should have_db_column(:post_id).of_type(:integer) }
 end
